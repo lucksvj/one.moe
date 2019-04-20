dc9f6da4d9361073fac350f3be60aec49d3d036f
lucksvj <48983490+lucksvj@users.noreply.github.com>
2019-04-20 14:52:26 +0800

    Add files via upload

--- 1902/微博/js/index.js
+++ 1902/微博/js/index.js
@@ -26,7 +26,11 @@ Index.prototype = {
 		 this.aBtn=document.querySelector("#btn");
 		 this.tet =document.querySelector("#test");
 		 this.tat=document.querySelector("#bust");
-		 this.box=document.querySelector(".box")
+		 this.box=document.querySelector(".bBox");
+		 this.ul=document.querySelector("ul");
+		 this.date = new Date();
+	   this.time = this.date.toLocaleString();
+			console.log(this.time)
 		 
 		this.btnClick();
 	},
@@ -34,6 +38,8 @@ Index.prototype = {
 	//点击button 时 需要实现的功能
 	btnClick: function(){
 		this.btn.onclick = () => {
+			this.tat.value="";
+			this.tet.value="";
 			//把modal插入页面中
 			document.body.appendChild(this.modal);
 			//让登陆框显示
@@ -45,7 +51,9 @@ Index.prototype = {
 	},
  	
  Cntent:function(){
+
      this.aBtn.onclick=()=>{
+			 console.log(1);
 		 //点击发布发布框消失
 		 this.container.style.display = "none";
 		 //模态框消失
@@ -53,9 +61,9 @@ Index.prototype = {
 		 //获取输入框的值
 		var aTxt = this.tat.value;
 		var bTxt= this.tet.value;
-		this.box.innerHTML = "用户名:" + bTxt + "<br/>" + "发布内容：" + aTxt + "<br/>"+"<div class='dele'>撤销</div>"
+		this.box.innerHTML = "用户名:" + bTxt + "<br/>" + "发布内容：" + aTxt + "<br/>"+this.time+ "<br/>"+"<div class='dele'>撤销</div>";
 		this.box.style.display="block";
-		
+
 	 }
 	 this.Revoke();
 },
@@ -76,10 +84,32 @@ Index.prototype = {
 			console.log(target)
 			if(target.className === "dele"){
 				this.container.style.display = "none";
-			}
-
+				this.box.remove();
+			}  
+    
 		
 		}
-}
+		this.monltor();
+	},
+	monltor: function(){
+     this.box.oncontextmenu=(e)=>{
+			 e=e ||event;
+			//阻止 浏览器右键菜单的弹出
+			if (e.preventDefault){
+					e.preventDefault();
+			}	 else{
+					e.returnValue = false;
+			}
+			this.ul.style.display="block";
+			this.ul.style.left= e.clientX + "px";
+			this.ul.style.top=e.clientY + "px";
+			this.ul.style.background="#fff";
+		  this.ul.onclick=()=>{
+			this.box.style.display = "none";
+			this.ul.style.display = "none";
+			}
+		
+		 }
+	}
 }
 new Index();
